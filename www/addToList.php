3a538a550d6e1791d773bedd6a894ca381fe0243
ec2-user <ec2-user@ip-172-31-21-211.ap-southeast-2.compute.internal>
2014-04-09 02:25:59 -0400

    Changed email addresses to a seperate php include

--- www/addToList.php
+++ www/addToList.php
@@ -1,23 +1,19 @@
 <?php
 $address = $_SERVER["REMOTE_ADDR"];
 $email = $_POST["emailaddress"];
-$hostnamefile = "/var/www/html/output/hostnames";
+$hostnamefile = "/usr/share/nginx/html/output/hostnames";
 $tempfile = "/tmp/hostnames";
-switch ($email) {
-	case "jenniferhunter@gmail.com":
-	case "cameroncros@gmail.com":
-	case "dark.violin.angel@gmail.com":
-	case "kieran.macfarlane@gmail.com":
+include "emailAddresses.php";
+
+if (in_array($email, $users))
+{
 	$safeemail = preg_replace('/[^A-Za-z0-9]/', '', $email);
 		echo system( "grep -v \"$safeemail\" $hostnamefile > $tempfile");
 		echo system( "echo \"$address #$safeemail\" >> $tempfile" );
 		copy($tempfile, $hostnamefile);
 		echo "Success, GO WATCH NETFLIX!";
-		break;
-	default:
-		echo "FUCK OFF";
-
-
+} else {
+	echo "Invalid Email";
 }
 
 ?>
